09953990914548d794d806e502135f9904f575ee
ttulov3 <87874297+ttulov3@users.noreply.github.com>
2022-05-26 11:38:42 -0300

    Add files via upload

--- OBJ DE ESTUDOS POO - ATARI PONG/main.js
+++ OBJ DE ESTUDOS POO - ATARI PONG/main.js
@@ -4,23 +4,38 @@ const context = canvas.getContext("2d")
 canvas.width = 800
 canvas.height = 600
 
-let y = 0
-let directionY = 1
+let y = 100
+let directionY = 0
 let barSize = 100
+
+document.addEventListener("keydown", ev =>{
+    if(ev.code == "KeyS" && !ev.repeat) {
+        return directionY = 5
+    }
+
+    if(ev.code == "KeyW" && !ev.repeat){
+        return directionY = -5
+    }
+})
+
+document.addEventListener("keyup", ev => {
+    if(ev.code == "KeyS" || ev.code == "KeyW") 
+       return directionY = 0
+})
+
+
 
 function mainLoop() {
     context.clearRect(0, 0, canvas.width, canvas.height)
-    context.fillRect(10, y, 100, 100)
+    context.fillRect(10, y, 20, barSize)
     y += directionY
 
     if (y <= 0 || y > canvas.height - barSize) {
-        directionY *= -1
+        directionY = 0
     }
 
     requestAnimationFrame(mainLoop)
 }
 
 requestAnimationFrame(mainLoop)
-
-
 document.body.append(canvas)
